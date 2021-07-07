8b2f067f482729605149b8559d1e3362a579961b
Sandeep Reddy U <sandeep@regology.com>
2021-07-07 13:59:54 +0530

    Display sample data in table. Styling changes.

--- assets-app/src/components/table.tsx
+++ assets-app/src/components/table.tsx
@@ -1,22 +1,42 @@
+import assetsData from "../data/sampleData.json";
+
+enum AssetClass {
+  CREDIT = "Credit",
+  EQUITIES = "Equities",
+  MACRO = "Macro",
+}
+interface Asset {
+  ticker: string;
+  price: number;
+  assetClass: AssetClass;
+}
+
 export default function Table() {
+  const data = assetsData as Asset[];
   return (
     <section>
-      <table>
-        <thead>
-          <tr>
-            <th>Asset Class</th>
-            <th>Price</th>
-            <th>Ticker</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>ALPHA</td>
-            <td>3150.67</td>
-            <td>Credit</td>
-          </tr>
-        </tbody>
-      </table>
+      {data.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Asset Class</th>
+              <th>Price</th>
+              <th>Ticker</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((asset) => (
+              <tr>
+                <td>{asset.assetClass}</td>
+                <td>{asset.price}</td>
+                <td>{asset.ticker}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        "No assets found"
+      )}
     </section>
   );
 }
