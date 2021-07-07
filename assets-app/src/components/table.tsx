d624b872f0dd484c3591ba2bed113be434a39229
Sandeep Reddy U <sandeep@regology.com>
2021-07-07 12:56:15 +0530

    Added header and table components. Added Sass support to the project. Scss cleanup.

---
+++ assets-app/src/components/table.tsx
@@ -0,0 +1,22 @@
+export default function Table() {
+  return (
+    <section>
+      <table>
+        <thead>
+          <tr>
+            <th>Asset Class</th>
+            <th>Price</th>
+            <th>Ticker</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ALPHA</td>
+            <td>3150.67</td>
+            <td>Credit</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+}
